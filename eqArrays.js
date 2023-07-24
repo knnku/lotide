e7842e6456be1c9705b187aab8a62e3d218f8635
Kenniku Tolato <kenniku_you@ymail.com>
2023-07-24 04:32:14 +0000

    Modified eqArrays to correct logic.

--- eqArrays.js
+++ eqArrays.js
@@ -1,4 +1,4 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -6,32 +6,23 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(array1, array2) {
+const eqArrays = function (array1, array2) {
   if (array1.length !== array2.length) {
     console.log("Please input two arrays with the same length.");
     return false;
   }
-  let result = "";
-
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      result = false;
-    }
-    if (array1[i] === array2[i]) {
-      result = true;
+      return false;
     }
   }
-  return result;
-
+  return true;
 };
 
-/*
 eqArrays([1, 2, 3], [1, 2, 3]); // => true
 eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
 eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-*/
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
