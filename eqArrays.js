3dc58a9fe55221ebd631896ef560f5d8a9899623
Kenniku Tolato <kenniku_you@ymail.com>
2023-08-04 05:08:30 +0000

    Add eqArrays.

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function (array1, array2) {
   if (array1.length !== array2.length) {
     console.log("Please input two arrays with the same length.");
@@ -19,10 +11,4 @@ const eqArrays = function (array1, array2) {
   return true;
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+module.exports = eqArrays;
