7446feb09993659442be383ada20704c1c257ab0
Kenniku Tolato <kenniku_you@ymail.com>
2023-08-05 04:39:26 +0000

    Refactor tail and head to test via mocha and chai.

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,20 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+const assert = require("chai").assert;
+const head = require("../head");
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), undefined); 
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5]), 5);
+// assertEqual(head([]), undefined);
+
+describe("#head", () => {
+  it("returns 5 for [5,6,7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+});
+
+
+describe("#head", () => {
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+})
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,17 @@
-const assertEqual = require('../assertEqual');
-const tail = require('../tail')
-
+const assert = require('chai').assert;
+const tail = require("../tail");
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!s
-assertEqual(tail(words), ["Lighthouse", "Labs"]) // Fail, can't compare arrays
 
 
+describe("#tail", () => {
+  it("should return Lighthouse, Labs", () => {
+    assert.deepEqual(tail(words), ["Lighthouse", "Labs"]);
+  });
+});
 
+describe("#tail", () => {
+  it("should return original array length: 3", () => {
+    assert.strictEqual(words.length, 3);
+  });
+});
